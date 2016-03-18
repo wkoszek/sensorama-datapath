a7f3abebacd1f6ec6593a78723cdd97f6d3937f2
Wojciech A. Koszek <wojciech@koszek.com>
2016-03-17 23:22:55 -0700

    Fix minor issues.

--- backend/upload.php
+++ backend/upload.php
@@ -1,7 +1,9 @@
 <?php
+date_default_timezone_set("UTC");
 
 $putdata = fopen("php://input", "r");
-$filename = strftime("sensorama_data/%Y%m%d.%H%s" . random());
+$filename = strftime("sensorama_data/%Y%m%d.%H%M%S." . rand());
+print $filename . "\n";
 $fp = fopen($filename, "w");
 while ($data = fread($putdata, 1024)) {
 	fwrite($fp, $data);
